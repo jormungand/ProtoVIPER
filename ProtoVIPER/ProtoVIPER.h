98a5b5632505235cc37a72c76ad27746106b6f0b
Ilya Stroganov <ilya.stroganov@ooma.com>
2019-04-26 12:15:24 +0300

    Initial Commit

---
+++ ProtoVIPER/ProtoVIPER.h
@@ -0,0 +1,19 @@
+//
+//  ProtoVIPER.h
+//  ProtoVIPER
+//
+//  Created by Ilya Stroganov on 4/26/19.
+//  Copyright © 2019 Ilya Stroganov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProtoVIPER.
+FOUNDATION_EXPORT double ProtoVIPERVersionNumber;
+
+//! Project version string for ProtoVIPER.
+FOUNDATION_EXPORT const unsigned char ProtoVIPERVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProtoVIPER/PublicHeader.h>
+
+
